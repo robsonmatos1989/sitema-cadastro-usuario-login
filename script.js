0fc05be5c5e05edcdc3445e3f5d310dbb0da18bb
Robson Matos <nosbor_album@hotmail.com>
2023-06-30 15:39:18 -0300

    Testando a api

--- script.js
+++ script.js
@@ -20,3 +20,34 @@ function nameValidate(){
 		removeError(0);
 	}
 }
+
+
+
+
+function consultaEndereco(){
+	let cep = document.querySelector('#f-cep').value;
+	if(cep.length !== 8){
+		alert('CEP Inválido');
+		return;
+	}
+	
+	
+		let url = `https://viacep.com.br/ws/${cep}/json/`;
+
+		fetch(url).then(function(response){
+			response.json().then(function(data){
+				console.log(data);
+				mostrarEndereco(data);
+
+			})
+		});
+	}
+
+function mostrarEndereco(dados){
+	let resultado = document.querySelector('#resultado');
+
+	resultado.innerHTML = `<p>Endereço: ${dados.logradouro}</p>	
+							<p>Complemento: ${dados.complemento}</p> 
+							<p>Bairro: ${dados.bairro}</p> 
+							<p>Cidade: ${dados.localidade} - ${dados.uf}</p>`
+}
